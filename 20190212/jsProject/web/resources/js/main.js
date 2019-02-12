bc848ebf62b2d2b824665c08bc867fe068db7c8e
Seokho Youn <axlover@hotmail.com>
2019-02-12 21:11:06 +0900

    javascript 실습

---
+++ 20190212/jsProject/web/resources/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * 2019/02/12 작성 자바스크립트 테스트
+ */
+
+function testShow(){
+	window.alert("main.js 안에있는 testShow() 실행됨");
+}
+
+function changeImage(){
+	document.getElementById("image1").src = "/testj/resources/images/Android.gif";
+}
